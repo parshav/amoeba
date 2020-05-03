5df19610e99e4a36d17af61b7e3f8e120da7e0e3
Parshav <parshav@live.com>
2020-05-03 15:59:53 -0500

    Finally working with rule 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,31 +1,71 @@
-import kotlin.collections.LinkedHashMap
-
 const val ANSI_BLUE = "\u001B[34m"
 const val ANSI_RESET = "\u001B[0m"
 const val ANSI_GREEN = "\u001B[32m"
 const val maxSize = 10
 
+var prevData = mutableMapOf<Int, Int>()
+val rulesMap = mutableMapOf<Int, Int>() // rules to the value they hold
+
 fun main() {
 
+    rules() // initial rules
     val mid = maxSize / 2
+    val currentData = mutableMapOf<Int, Int>()
 
-    val data = mutableMapOf<Int, Char>()
-
-    data[mid] = 'O'
+    // check if should seed mid or not depending on all 0 rule.
+//    prevData[mid] = 1
 
     for (i in 0..maxSize) {
         for (j in 0..maxSize) {
 
-            data[j]?.let {
-                pXorO(it)
-            } ?: pX()
-
+            if (i == 0) {
+                prevData[j]?.let {
+                    pXorO(it)
+                    currentData[j] = it
+                } ?: run {
+                    pO()
+                    currentData[j] = 0
+                }
+            } else {
+                val value = getValueFromPosition(j)
+                currentData[j] = value
+                pXorO(value)
+            }
+        }
+//        prevData = currentData
+        currentData.forEach {
+            prevData[it.key] = it.value
         }
         println()
     }
 }
 
-fun pX() = print("$ANSI_GREEN X $ANSI_RESET")
-fun pO() = print("$ANSI_BLUE O $ANSI_RESET")
+fun getValueFromPosition(currentPosition: Int): Int {
+    return if (currentPosition - 1 < 0 || currentPosition + 1 > maxSize) {
+        0 // if edges, return empty slate
+    } else {
+        val a = prevData[currentPosition - 1]!!
+        val b = prevData[currentPosition]!!
+        val c = prevData[currentPosition + 1]!!
 
-fun pXorO(char: Char) = if (char == 'X') pX() else pO()
+        val ruleValueCode = a * 100 + (b * 10) + c
+        rulesMap[ruleValueCode]!! // rule value code should always equate
+    }
+}
+
+fun rules() {
+    rulesMap[111] = 0
+    rulesMap[110] = 0
+    rulesMap[101] = 0
+    rulesMap[100] = 0
+    rulesMap[11] = 0
+    rulesMap[10] = 0
+    rulesMap[1] = 0
+    rulesMap[0] = 1
+}
+
+
+fun p1() = print("$ANSI_BLUE 1 $ANSI_RESET")
+fun pO() = print("$ANSI_GREEN O $ANSI_RESET")
+
+fun pXorO(int: Int) = if (int == 1) p1() else pO()
